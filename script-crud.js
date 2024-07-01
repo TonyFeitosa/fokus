0f877cf034524d2555a1d0a85659ce72a0ee5ae7
TonyFeitosa <tonypereira697@gmail.com>
2024-07-01 08:34:23 -0300

    atualizando a localstorage

--- script-crud.js
+++ script-crud.js
@@ -5,6 +5,10 @@ const ulTarefas = document.querySelector('.app__section-task-list')
 
 const tarefas = JSON.parse(localStorage.getItem('tarefas')) || []
 
+function atualizarTarefas () {
+    localStorage.setItem('tarefas', JSON.stringify(tarefas))
+}
+
 function criarElementoTarefa(tarefa) {
     const li = document.createElement('li')
     li.classList.add('app__section-task-list-item')
@@ -27,6 +31,8 @@ function criarElementoTarefa(tarefa) {
     botao.onclick = () => {
         const novaDescrcao = prompt('Qual é o novo nome da tarefa?')
         paragrafo.textContent = novaDescrcao
+        tarefa.descricao = novaDescrico
+        atualizarTarefas()
     }
 
     const imagemBotao = document.createElement('img')
@@ -51,7 +57,7 @@ formAdicionarTarefa.addEventListener('submit', (evento) => {
     }
     tarefas.push(tarefa)
     const elementoTarefa = criarElementoTarefa(tarefa)
-    localStorage.setItem('tarefas', JSON.stringify(tarefas))
+    atualizarTarefas()
     textarea.value = ''
     formAdicionarTarefa.classList.add('hidden')
 });
